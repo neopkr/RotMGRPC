78ef734b7c71e6cbfd955fe00f7edc04ae076e5b
Joaquín <gmdneruu@gmail.com>
2022-03-18 19:53:42 -0300

    [Major Changes] Lang change
    Switched from JS to Py for the sake of preference (and easier environment).

---
+++ main.py
@@ -0,0 +1,85 @@
+import time, os
+from pypresence import Presence
+import requests as r
+from datetime import datetime
+
+clientID = "954113378438246461"
+rpc = Presence(clientID)
+rpc.connect()
+
+classes = {
+  'Rogue': 'rogue-icon',
+  'Archer': 'archer-icon',
+  'Wizard': 'wizard-icon',
+  'Priest': 'priest-icon',
+  'Warrior': 'warrior-icon',
+  'Knight': 'knight-icon',
+  'Paladin': 'paladin-icon',
+  'Assassin': 'assassin-icon',
+  'Necromancer': 'necromancer-icon',
+  'Huntress': 'huntress-icon',
+  'Mystic': 'mystic-icon',
+  'Trickster': 'trickster-icon',
+  'Sorcerer': 'sorcerer-icon',
+  'Ninja': 'ninja-icon',
+  'Samurai': 'samurai-icon',
+  'Bard': 'bard-icon',
+  'Summoner': 'summoner-icon',
+  'Kensei': 'kensei-icon'
+}
+
+now = datetime.utcnow()
+elapsed = int((now - datetime(1970, 1, 1)).total_seconds())
+
+def _getGame():
+  pid = os.getpid()
+  return pid
+
+print('^*------------RotMG-RPC------------*^')
+print('Created by: ether#8677 (IGN: Neruncio) & neokeee#9998 (IGN: Neopkr)')
+print('This app loads data from RealmEye API, not from the game data.')
+player = input('** IGN (In-Game Name): ')
+print(f'** Loading IGN: {player}')
+
+url = f"https://nightfirec.at/realmeye-api/?player={player}&filter=player+characters+class+fame+rank"
+
+while True:
+  res = r.get(url)
+  data = res.json()
+
+  charactersList = data['characters']
+  playingAs = charactersList[0];
+
+  def _getClass():
+    return classes[playingAs['class']]
+    
+  ign = data['player']
+  basefame = playingAs['fame']
+  starQty = data['rank']
+
+  def rank():
+    if starQty >= 0 and starQty <= 17:
+      return "lightbluestar-icon"
+    elif starQty >= 18 and starQty <= 35:
+      return "bluestar-icon"
+    elif starQty >= 36 and starQty <= 53:
+      return "redstar-icon"
+    elif starQty >= 54 and starQty <= 71:
+      return "orangestar-icon"
+    elif starQty >= 72 and starQty <= 89:
+      return "yellowstar-icon"
+    else:
+      return "whitestar-icon"
+
+  rpc.update(
+    pid = _getGame(),
+    state = f"Playing as: {playingAs['class']}",
+    details = f"IGN: {ign}",
+    start = str(elapsed),
+    large_image = _getClass(),
+    large_text = f'{basefame} BF',
+    small_image = rank(),
+    small_text = str(starQty)
+  )
+    
+  time.sleep(60)
